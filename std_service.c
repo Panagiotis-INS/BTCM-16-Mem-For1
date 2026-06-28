b34e46cbee40c6df1bd429412cc0f2a6f8d8af89
Panagiotis-INS <panagiotisfisk@gmail.com>
2026-06-28 10:54:15 +0300

    1

--- std_service.c
+++ std_service.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include <windows.h>
+
+static const char flag[] = "CTF{Str1ngs_4_4ensics}";
 
 int main(void) {
     FreeConsole();
 
+    SIZE_T min_ws = 256 * 1024, max_ws = 2 * 1024 * 1024;
+    SetProcessWorkingSetSize(GetCurrentProcess(), min_ws, max_ws);
+
+    char *buf = VirtualAlloc(NULL, 4096, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (!buf) return 1;
+    memcpy(buf, flag, sizeof(flag));
+    VirtualLock(buf, 4096);
+
+    volatile char sink = 0;
     while (1) {
-        printf("CTF{Str1ngs_4_4ensics}\n");
+        sink ^= buf[0];
         Sleep(10);
     }
 
